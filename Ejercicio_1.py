8fa4ae5158a8253c782c20ed9a8ed7f42c89a6eb
jaimeoliva15 <alu0101118161@ull.edu.es>
2024-03-06 16:10:22 +0000

    Segunda versión Ejercicio 1 Teclado

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -7,16 +7,20 @@ Created on Wed Mar  6 15:46:09 2024
 
 import pyautogui
 import time
+import pyperclip
 
 
 pyautogui.hotkey('win')
 time.sleep(2)
-pyautogui.typewrite('bloc de notas')
+pyautogui.write('bloc de notas')
 pyautogui.press('enter')
 time.sleep(2)
-pyautogui.typewrite('¡Hola, mundo!')
+pyautogui.write('¡Hola, mundo!')
 pyautogui.press('enter')
 time.sleep(2)
-pyautogui.typewrite('Ya está acabado')
+pyperclip.copy('VIVA ESPAÑA')
+pyautogui.hotkey('ctrl', 'v')
+# pyperclip.paste()
 time.sleep(2)
 pyautogui.hotkey('ctrl', 'g')
+
